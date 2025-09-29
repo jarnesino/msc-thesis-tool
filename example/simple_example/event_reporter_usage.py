b1939b4b900b75a2a711e49773c55609ae6cdf30
Julian Arnesino <julianezequielarnesino@gmail.com>
2025-09-29 19:40:01 -0300

    Extend simple example log

--- example/simple_example/event_reporter_usage.py
+++ example/simple_example/event_reporter_usage.py
@@ -22,25 +22,44 @@ class Clock:
 clock = Clock()
 
 report_file_name = "main_log.txt"
-with open(report_file_name, "w+") as report_file:
-    event_reporter = EventReporter(report_file)
 
+
+def log_test_run_branch():
+    event_reporter.report_variable_value_assigned(
+        "results", "results OK", clock.current()
+    )
+    event_reporter.report_variable_value_assigned(
+        "signatures", "supervisor1,supervisor2", clock.current()
+    )
+    event_reporter.report_task_started("stop_coolant_pump", clock.current())
+    event_reporter.report_variable_value_assigned("stage", "1", clock.current())
+    event_reporter.report_task_finished("stop_coolant_pump", clock.current())
+    event_reporter.report_checkpoint_reached("log_test_run_results", clock.current())
+
+
+def log_test_run_branch_declarations():
     event_reporter.report_declared_variable("results", "string", clock.current())
     event_reporter.report_declared_variable("signatures", "string", clock.current())
 
+
+def log_common_branch_declarations():
     event_reporter.report_declared_variable("fail_safe_lock", "bool", clock.current())
+    event_reporter.report_declared_variable("stage", "int", clock.current())
+    event_reporter.report_declared_variable("pressure", "int", clock.current())
+    event_reporter.report_declared_variable("temperature", "int", clock.current())
+
+
+def log_common_branch():
     event_reporter.report_variable_value_assigned(
         "fail_safe_lock", "False", clock.current()
     )
 
-    event_reporter.report_declared_variable("stage", "int", clock.current())
     event_reporter.report_variable_value_assigned("stage", "1", clock.current())
-
     event_reporter.report_task_started(
         "initiate_pressure_measurements", clock.current()
     )
     event_reporter.report_checkpoint_reached("control_failsafe", clock.current())
-    event_reporter.report_declared_variable("pressure", "int", clock.current())
+
     event_reporter.report_variable_value_assigned("pressure", "156", clock.current())
     event_reporter.report_component_event(
         "display", "display_write,156", clock.current()
@@ -48,16 +67,13 @@ with open(report_file_name, "w+") as report_file:
     event_reporter.report_task_finished(
         "initiate_pressure_measurements", clock.current()
     )
-
     event_reporter.report_task_started("start_coolant_pump", clock.current())
     event_reporter.report_variable_value_assigned("stage", "2", clock.current())
     event_reporter.report_task_finished("start_coolant_pump", clock.current())
-
     event_reporter.report_task_started(
         "initiate_temperature_measurements", clock.current()
     )
     event_reporter.report_checkpoint_reached("control_failsafe", clock.current())
-    event_reporter.report_declared_variable("temperature", "int", clock.current())
     event_reporter.report_variable_value_assigned("temperature", "617", clock.current())
     event_reporter.report_component_event(
         "display", "display_write,156", clock.current()
@@ -66,15 +82,12 @@ with open(report_file_name, "w+") as report_file:
         "initiate_temperature_measurements", clock.current()
     )
 
-    event_reporter.report_variable_value_assigned(
-        "results", "results OK", clock.current()
-    )
-    event_reporter.report_variable_value_assigned(
-        "signatures", "supervisor1,supervisor2", clock.current()
-    )
 
-    event_reporter.report_task_started("stop_coolant_pump", clock.current())
-    event_reporter.report_variable_value_assigned("stage", "1", clock.current())
-    event_reporter.report_task_finished("stop_coolant_pump", clock.current())
+with open(report_file_name, "w+") as report_file:
+    event_reporter = EventReporter(report_file)
 
-    event_reporter.report_checkpoint_reached("log_test_run_results", clock.current())
+    log_common_branch_declarations()
+    log_test_run_branch_declarations()
+    log_common_branch()
+    log_test_run_branch()
+    log_common_branch()
