72e1cc03aef0bdaff73debb6383df11376c789ca
Julian Arnesino <julianezequielarnesino@gmail.com>
2025-09-29 19:29:51 -0300

    Extend simple example log

--- example/simple_example/event_reporter_usage.py
+++ example/simple_example/event_reporter_usage.py
@@ -45,3 +45,32 @@ with open(report_file_name, "w+") as report_file:
     event_reporter.report_task_finished(
         "initiate_pressure_measurements", clock.current()
     )
+
+    event_reporter.report_task_started(
+        "start_coolant_pump", clock.current()
+    )
+    event_reporter.report_variable_value_assigned("stage", "2", clock.current())
+    event_reporter.report_task_finished(
+        "start_coolant_pump", clock.current()
+    )
+
+    event_reporter.report_task_started(
+        "initiate_temperature_measurements", clock.current()
+    )
+    event_reporter.report_checkpoint_reached("control_failsafe", clock.current())
+    event_reporter.report_declared_variable("temperature", "int", clock.current())
+    event_reporter.report_variable_value_assigned("temperature", "617", clock.current())
+    event_reporter.report_component_event(
+        "display", "display_write,156", clock.current()
+    )
+    event_reporter.report_task_finished(
+        "initiate_temperature_measurements", clock.current()
+    )
+
+    event_reporter.report_task_started(
+        "stop_coolant_pump", clock.current()
+    )
+    event_reporter.report_variable_value_assigned("stage", "1", clock.current())
+    event_reporter.report_task_finished(
+        "stop_coolant_pump", clock.current()
+    )
