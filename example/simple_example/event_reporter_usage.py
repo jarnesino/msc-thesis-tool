b2d7a096c32277c6737346ea240f3cebf74de5b1
Julian Arnesino <julianezequielarnesino@gmail.com>
2025-09-29 19:45:57 -0300

    Extend simple example log with test run branch, real run branch and loops

--- example/simple_example/event_reporter_usage.py
+++ example/simple_example/event_reporter_usage.py
@@ -24,29 +24,16 @@ clock = Clock()
 report_file_name = "main_log.txt"
 
 
-def log_test_run_branch():
-    event_reporter.report_variable_value_assigned(
-        "results", "results OK", clock.current()
-    )
-    event_reporter.report_variable_value_assigned(
-        "signatures", "supervisor1,supervisor2", clock.current()
-    )
-    event_reporter.report_task_started("stop_coolant_pump", clock.current())
-    event_reporter.report_variable_value_assigned("stage", "1", clock.current())
-    event_reporter.report_task_finished("stop_coolant_pump", clock.current())
-    event_reporter.report_checkpoint_reached("log_test_run_results", clock.current())
-
-
-def log_test_run_branch_declarations():
-    event_reporter.report_declared_variable("results", "string", clock.current())
-    event_reporter.report_declared_variable("signatures", "string", clock.current())
-
-
 def log_common_branch_declarations():
     event_reporter.report_declared_variable("fail_safe_lock", "bool", clock.current())
     event_reporter.report_declared_variable("stage", "int", clock.current())
     event_reporter.report_declared_variable("pressure", "int", clock.current())
     event_reporter.report_declared_variable("temperature", "int", clock.current())
+
+
+def log_test_run_branch_declarations():
+    event_reporter.report_declared_variable("results", "string", clock.current())
+    event_reporter.report_declared_variable("signatures", "string", clock.current())
 
 
 def log_common_branch():
@@ -83,11 +70,41 @@ def log_common_branch():
     )
 
 
+def log_test_run_branch():
+    event_reporter.report_variable_value_assigned(
+        "results", "results OK", clock.current()
+    )
+    event_reporter.report_variable_value_assigned(
+        "signatures", "supervisor1,supervisor2", clock.current()
+    )
+    event_reporter.report_task_started("stop_coolant_pump", clock.current())
+    event_reporter.report_variable_value_assigned("stage", "1", clock.current())
+    event_reporter.report_task_finished("stop_coolant_pump", clock.current())
+    event_reporter.report_checkpoint_reached("log_test_run_results", clock.current())
+
+
+def log_real_run_branch():
+    event_reporter.report_checkpoint_reached(
+        "control_pressure_manually", clock.current()
+    )
+    event_reporter.report_checkpoint_reached(
+        "control_temperature_manually", clock.current()
+    )
+    event_reporter.report_task_started("withdraw_control_rods", clock.current())
+    event_reporter.report_variable_value_assigned("stage", "3", clock.current())
+    event_reporter.report_task_finished("withdraw_control_rods", clock.current())
+
+
 with open(report_file_name, "w+") as report_file:
     event_reporter = EventReporter(report_file)
 
+    LOOPS = 3
+
     log_common_branch_declarations()
     log_test_run_branch_declarations()
-    log_common_branch()
-    log_test_run_branch()
-    log_common_branch()
+
+    for _loop in range(LOOPS):
+        log_common_branch()
+        log_test_run_branch()
+        log_common_branch()
+        log_real_run_branch()
