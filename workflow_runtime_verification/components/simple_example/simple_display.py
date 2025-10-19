15a7895e0da4058341ab42d3e188de38ed944733
Julian Arnesino <julianezequielarnesino@gmail.com>
2025-10-20 00:37:50 +0900

    Expand on the functionality of the simple display

--- workflow_runtime_verification/components/simple_example/simple_display.py
+++ workflow_runtime_verification/components/simple_example/simple_display.py
@@ -6,15 +6,16 @@ from workflow_runtime_verification.components.component import Component
 class SimpleDisplay(Component):
     def __init__(self):
         super().__init__()
+        self._state = {"data": np.uint16(0)}
 
     def write(self, data: np.uint16):
-        pass
+        self._state["data"] = data
 
     def stop(self):
         pass
 
     def state(self):
-        return {}
+        return self._state
 
     def exported_functions(self):
         return {"display_write": self.write}
